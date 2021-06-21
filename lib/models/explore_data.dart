afd65551139c9265fe6bfad57adfb05c6634d8e0
legally free <legallyfreee@gmail.com>
2021-06-20 23:11:12 -0300

    Initial commit

---
+++ lib/models/explore_data.dart
@@ -0,0 +1,8 @@
+import 'models.dart';
+
+class ExploreData {
+  List<ExploreRecipe> todayRecipes;
+  List<Post> friendPosts;
+
+  ExploreData(this.todayRecipes, this.friendPosts);
+}
---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import '../api/mock_fooderlich_service.dart';
+import '../components/components.dart';
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data.todayRecipes),
+              const SizedBox(height: 16),
+              Container(height: 400, color: Colors.green),
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
